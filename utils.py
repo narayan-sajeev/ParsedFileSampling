84fbcec741164dbcad5967f2ea2e2d0618b1fbd7
Narayan Sajeev <narayansajeev1@gmail.com>
2024-02-05 18:46:49 -0500

    Refactor code

--- utils.py
+++ utils.py
@@ -507,7 +507,7 @@ def process_df(df):
 
 
 def print_file_path():
-    path = 'open %s' % get_path()
+    path = 'open \'%s\'' % get_path()
     fname = path.split('/')[-1]
     print(fname)
     return path
