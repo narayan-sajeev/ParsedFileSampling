586251c0d5052dae0b6631c653a166d879db3cea
Narayan Sajeev <narayansajeev1@gmail.com>
2024-05-06 17:30:52 -0400

    Refactor code

--- main.py
+++ main.py
@@ -14,7 +14,5 @@ parsed_df, raw_df = init(PROV, FILE_NAMES, FILE_NUM, col_headers)
 
 if df_exists(raw_df):
     parsed_df, raw_df = drop_common_cols(parsed_df, raw_df)
-    results(parsed_df, raw_df)
 
-else:
-    results(parsed_df, None)
+results(parsed_df, raw_df)
