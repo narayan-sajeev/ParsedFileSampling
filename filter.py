f5d3348a0ee8dbc5775d62acdc79f5862793af91
Narayan Sajeev <narayansajeev1@gmail.com>
2024-06-17 13:56:42 -0400

    Add filter function to remove empty rows

---
+++ filter.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+def filter_df(parsed_df):
+    # Create new empty DataFrame with the same columns as the input DataFrame
+    filtered = pd.DataFrame(columns=parsed_df.columns)
+    # Iterate through each row in the DataFrame
+    for index, row in parsed_df.iterrows():
+        # Check if the row is valid
+        if check_row(row):
+            # Append the row to the new DataFrame
+            filtered = pd.concat([filtered, pd.DataFrame(row).T])
+
+    # Return the new DataFrame
+    return filtered
+
+
+def has_column(row, c):
+    # Check if the column exists in the row
+    if c in row:
+        # Check if the value in the column is not NaN
+        try:
+            if pd.isna(row[c]):
+                return False
+            else:
+                return True
+        except TypeError:
+            return True
+    return False
+
+
+def check_row(row):
+    # Define the vital columns
+    vital_a = ['food_name', 'product_classification']
+    vital_b = ['sampled_location_name', 'sampled_location_address', 'sampled_location_province']
+    vital_c = ['manufacturer_name', 'manufacturer_address']
+
+    # Check if the row contains any of the vital columns
+    if any(has_column(row, c) for c in vital_a):
+        return True
+    if any(has_column(row, c) for c in vital_b) and any(has_column(row, c) for c in vital_c):
+        return True
+    return False
