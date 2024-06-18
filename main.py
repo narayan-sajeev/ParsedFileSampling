2ed069844b926047f4a1172850dcfea2784b6329
Narayan Sajeev <narayansajeev1@gmail.com>
2024-06-17 21:09:50 -0400

    Remove filtered

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from filter import filter_df
 from utils import *
 
 PROV = 'Dongguan_Guangdong_msb_20230331'
@@ -15,6 +14,3 @@ if df_exists(raw_df):
     parsed_df, raw_df = remove_common_cols(parsed_df, raw_df)
 
 results(parsed_df, raw_df)
-
-filtered = filter_df(parsed_df)
-print(filtered)
